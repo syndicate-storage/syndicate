141746d4bcb88cf03dacf5460c2c4b0f5bf40fb1
debian <debian@debian>
2013-06-13 12:17:44 -0400

    Initial files added for web frontend. - John

---
+++ web/syndicatetestweb/accounts/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+                       url(r'^login/?', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}, name='login'),
+                       url(r'^logout/?', 'django.contrib.auth.views.logout_then_login', name='logout'),
+
+    # Examples:
+    # url(r'^$', 'synweb.views.home', name='home'),
+    # url(r'^synweb/', include('synweb.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
