eab528fc037122c66fd63b4677f1e5a02e7d86eb
jcnelson <jcnelson@cs.princeton.edu>
2013-06-17 13:31:49 -0400

    Add ms SConscript

--- ms/tests/__init__.py
+++ ms/tests/__init__.py
@@ -1,6 +1,6 @@
 import setup
-import create_msentry
-import read_msentry
-import delete_msentry
-import update_msentry
-import resolve
+#import create_msentry
+#import read_msentry
+#import delete_msentry
+#import update_msentry
+#import resolve
