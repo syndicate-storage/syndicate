19f5132a98cdf119bbcc4f7dc8e476c3a3512fae
debian <jwhelchel@debian>
2013-06-17 14:42:33 -0400

    Reorganized structure of web/ to be within ms/.

--- web/syndicatetestweb/home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('',
-                       url(r'^$', 'home.views.home'),
-                       url(r'^allvolumes/+$', 'home.views.allvolumes'),
-                       url(r'^myvolumes/+$', 'home.views.myvolumes'),
-                       url(r'^settings/+$', 'home.views.settings'),
-                       url(r'^downloads/+$','home.views.downloads'),
-)
