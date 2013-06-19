74633cd85e309f326f7704749dbf5ceb212aed63
= <=>
2013-06-18 20:55:46 -0400

    Added RPM-generating scripts for Syndicate components and dependencies

--- pkg/scripts/libsyndicate.sh
+++ pkg/scripts/libsyndicate.sh
@@ -6,9 +6,10 @@
 PKG_ROOT=$1
 PKG_PKGOUT=$2
 PKG_REPO=$3
-PKG_VERSION="0.01"
-PKG_BUILD=$PKG_ROOT/out
-PKG_INSTALL=$PKG_ROOT/out/usr/local
+PKG_NAME="libsyndicate"
+PKG_VERSION="0.$(date +%Y\%m\%d\%H\%M\%S)"
+PKG_BUILD=$PKG_ROOT/out/libsyndicate
+PKG_INSTALL=$PKG_ROOT/out/libsyndicate/usr/
 
 PKG_DEPS="openssl curl uriparser protobuf" 
 
@@ -19,6 +20,10 @@ done
 
 source /usr/local/rvm/scripts/rvm
 
+rm -rf $PKG_BUILD
+rm -rf $PKG_INSTALL
+
+mkdir -p $PKG_BUILD
 mkdir -p $PKG_INSTALL
 
 pushd $PKG_REPO
@@ -28,5 +33,5 @@ scons libsyndicate
 scons libsyndicate-install DESTDIR=$PKG_INSTALL
 popd
 
-fpm --force -s dir -t rpm -v $PKG_VERSION -n libsyndicate $DEPARGS -d "libmicrohttpd > 0.9" -C $PKG_BUILD -p $PKG_PKGOUT --license proprietary --vendor PlanetLab --description "Syndicate common libraries and headers" $(ls $PKG_BUILD)
+fpm --force -s dir -t rpm -v $PKG_VERSION -n $PKG_NAME $DEPARGS -d "libmicrohttpd > 0.9" -C $PKG_BUILD -p $PKG_PKGOUT --license proprietary --vendor PlanetLab --description "Syndicate common libraries and headers" $(ls $PKG_BUILD)
 
---
+++ pkg/scripts/syndicate-UG.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+PKG_ROOT=$1
+PKG_PKGOUT=$2
+PKG_REPO=$3
+PKG_NAME="syndicate-UG"
+PKG_VERSION="0.$(date +%Y\%m\%d\%H\%M\%S)"
+PKG_BUILD=$PKG_ROOT/out/syndicate-UG
+PKG_INSTALL=$PKG_ROOT/out/syndicate-UG/usr
+
+PKG_DEPS="openssl curl uriparser protobuf fuse libsyndicate" 
+
+DEPARGS=""
+for pkg in $PKG_DEPS; do
+   DEPARGS="$DEPARGS -d $pkg"
+done
+
+source /usr/local/rvm/scripts/rvm
+
+rm -rf $PKG_BUILD
+rm -rf $PKG_INSTALL
+mkdir -p $PKG_BUILD
+mkdir -p $PKG_INSTALL
+
+pushd $PKG_REPO
+scons -c UG-install DESTDIR=$PKG_INSTALL
+scons -c UG
+scons UG
+scons UG-install DESTDIR=$PKG_INSTALL
+popd
+
+fpm --force -s dir -t rpm -v $PKG_VERSION -n $PKG_NAME $DEPARGS -d "libmicrohttpd > 0.9" -C $PKG_BUILD -p $PKG_PKGOUT --license proprietary --vendor PlanetLab --description "Syndicate User Gateways" $(ls $PKG_BUILD)
+
