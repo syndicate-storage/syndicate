8fea3b0a9009acba2a4e677abbb584e622c690cd
debian <jwhelchel@debian>
2013-06-19 10:57:27 -0400

    Adjusted internal filename references

--- ms/django_syndicate_web/wsgi.py
+++ ms/django_syndicate_web/wsgi.py
@@ -15,7 +15,7 @@ framework.
 """
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "syndicatetestweb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_syndicate_web.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
--- ms/manage.py
+++ ms/manage.py
@@ -4,8 +4,8 @@ import sys
 
 if __name__ == "__main__":
     sys.path.append(os.path.dirname(__file__))
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "syndicatetestweb.settings")
-    os.environ['DJANGO_SETTINGS_MODULE'] = 'syndicatetestweb.settings'
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_syndicate_web.settings")
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
