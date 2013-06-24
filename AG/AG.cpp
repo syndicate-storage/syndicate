573f684c18a834e59f0c571c7373a4ea384f37d7
wathsalav <wvithanage@gmail.com>
2013-06-23 21:05:15 -0400

    AG is now backed by drivers specific to the datasets. AG/drivers is the parent directory of driver modules. Drivers are loaded at runtime dynamically. Driver name should be specified in the config file using the key AG_GATEWAY_DRIVER_KEY.

---
+++ AG/AG.cpp
@@ -0,0 +1,16 @@
+/*
+   Copyright 2013 The Trustees of Princeton University
+   All Rights Reserved
+*/
+
+#include "AG-disk.h"
+#include "libgateway.h"
+
+
+int main( int argc, char** argv ) {
+   //Load the driver.
+   //Call init.
+   int rc = AG_main( argc, argv );
+   return rc;
+}
+
---
+++ AG/drivers/disk/driver.h
@@ -0,0 +1,8 @@
+#ifndef _DISK_DRIVER_H_
+#define _DISK_DRIVER_H_
+
+static int publish(const char *fpath, const struct stat *sb,
+	int tflag, struct FTW *ftwbuf);
+
+#endif //_DISK_DRIVER_H_
+
