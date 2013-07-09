2219f4d02ec4d4eb6d9ae898860cae08138e837e
jcnelson <jcnelson@cs.princeton.edu>
2013-07-08 22:15:37 -0400

    Put Syndicate data into /tmp/syndicate.PID by default

--- libsyndicate/libsyndicate.cpp
+++ libsyndicate/libsyndicate.cpp
@@ -101,13 +101,14 @@ static int md_runtime_init( int gateway_type, struct md_syndicate_conf* c, char 
    }
 
    // make sure we have data root and staging root
-   char* cwd = getenv( "HOME" );
-   char* data_root = md_fullpath( cwd, ".syndicate/data", NULL );
-   char* staging_root = md_fullpath( cwd, ".syndicate/staging", NULL );
-   char* logfile_path = md_fullpath( cwd, ".syndicate/access.log", NULL );
-   char* replica_logfile_path = md_fullpath( cwd, ".syndicate/replica.log", NULL );
-   char* RG_metadata_path = md_fullpath( cwd, ".syndicate/RG-metadata", NULL );
-   char* AG_metadata_path = md_fullpath( cwd, ".syndicate/AG-metadata", NULL );
+   char cwd[100];
+   sprintf(cwd, "/tmp/syndicate.%d", getpid() );
+   char* data_root = md_fullpath( cwd, "data", NULL );
+   char* staging_root = md_fullpath( cwd, "staging", NULL );
+   char* logfile_path = md_fullpath( cwd, "access.log", NULL );
+   char* replica_logfile_path = md_fullpath( cwd, "replica.log", NULL );
+   char* RG_metadata_path = md_fullpath( cwd, "RG-metadata", NULL );
+   char* AG_metadata_path = md_fullpath( cwd, "AG-metadata", NULL );
    
    if( c->data_root == NULL )
       c->data_root = strdup( data_root );
