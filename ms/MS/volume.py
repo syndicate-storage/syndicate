2d59c1c8a24b7c66798562335b99a4023667e6e4
jcnelson <jcnelson@cs.princeton.edu>
2013-07-23 03:31:57 -0400

    Identify Volumes by their ID as their primary key, not their name.  Also, when creating a Volume, atomically update the creating SyndicateUser to include the Volume in the read-only and read-write lists.  Additionally, remove redundant ListAll method.

--- ms/MS/volume.py
+++ ms/MS/volume.py
@@ -23,6 +23,8 @@ import random
 import string
 import logging
 
+from user import SyndicateUser
+
 # DEPRICATED
 VOLUME_SECRET_LENGTH = 256
 VOLUME_SECRET_SALT_LENGTH = 256
@@ -39,8 +41,33 @@ class VolumeIDCounter( storagetypes.Object ):
    def make_key_name( cls, **attrs ):
       return "VolumeIDCounter"
 
+   @classmethod
+   def __next_value( cls ):
+      # volume does not exist
+      vid_counter = VolumeIDCounter.get_or_insert( VolumeIDCounter.make_key_name(), value=0 )
+
+      vid_counter.value += 1
+
+      ret = vid_counter.value
+
+      vid_key = vid_counter.put()
+
+      return ret
+
+   @classmethod
+   def next_value( cls ):
+      return storagetypes.transaction( lambda: VolumeIDCounter.__next_value() )
+      
    
 
+def is_int( x ):
+   try:
+      y = int(x)
+      return True
+   except:
+      return False
+   
+   
 class Volume( storagetypes.Object ):
 
    HTTP_VOLUME_SECRET = "Syndicate-VolumeSecret"
@@ -91,22 +118,26 @@ class Volume( storagetypes.Object ):
       "volume_secret_salt",
       "volume_secret_salted_hash",
       "ag_ids",
-      "rg_ids"
+      "rg_ids",
+      "private"
    ]
 
    key_attrs = [
-      "name"
+      "volume_id"
    ]
 
+      
    validators = {
-      "name": (lambda cls, value: len( value.translate(dict((ord(char), None) for char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.")) ) == 0 )
+      "name": (lambda cls, value: len( value.translate(dict((ord(char), None) for char in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-.")) ) == 0 and not is_int(value) ),
+      "private": (lambda cls, value: type(value) is bool)
    }
 
    default_values = {
       "ag_ids": (lambda cls, attrs: []),
       "rg_ids": (lambda cls, attrs: []),
       "blocksize": (lambda cls, attrs: 61440), # 60 KB
-      "version": (lambda cls, attrs: 1)
+      "version": (lambda cls, attrs: 1),
+      "private": (lambda cls, attrs: True)
    }
 
       
@@ -139,19 +170,20 @@ class Volume( storagetypes.Object ):
 
       return True
       
-
+      
    @classmethod
-   def Create( cls, user, **kwargs ):
+   def Create( cls, username, **kwargs ):
       """
       Given volume data, store it.  Return the volume key.
-
+      If the user is given (as the owner), update the user atomically along with the create.
+      
       kwargs:
          name: str
          blocksize: int
          description: str
       """
 
-      kwargs['owner_id'] = user.owner_id
+      kwargs['owner_id'] = 0     # will look up user and fill with owner ID once we validate input.
       Volume.fill_defaults( kwargs )
 
       # DEPRICATED
@@ -179,39 +211,36 @@ class Volume( storagetypes.Object ):
          raise Exception( "Invalid values for fields: %s" % (", ".join( invalid )) )
 
 
-
       name = kwargs.get( "name" )
       blocksize = kwargs.get( "blocksize" )
       description = kwargs.get( "description" )
-      if "private" in kwargs:
-         if type(kwargs['private']) is bool:
-            private = kwargs['private']
-         else:
-            raise Exception( "Private must be a boolean value")
-      else:
-         private = False
-
-      volume_key = storagetypes.make_key( Volume, Volume.make_key_name( name=name ) )
-      volume = volume_key.get()
-      if volume != None and volume.volume_id > 0:
+      private = kwargs.get("private")
+      
+      existing_volumes = Volume.ListAll( {"Volume.name ==" : name} )
+      if len(existing_volumes) > 0:
          # volume already exists
          raise Exception( "Volume '%s' already exists" % name )
 
       else:
-         # volume does not exist
-         vid_counter = VolumeIDCounter.get_or_insert( VolumeIDCounter.make_key_name(), value=0 )
-         
-         vid_counter.value += 1
+         # Volume did not exist at the time of the query.
+         # Optimistically create it, and then verify that it is the only Volume with that name.
+         # Roll back if someone else created a Volume of the same name.
 
-         vid_future = vid_counter.put_async()
+         # get the user
+         user = SyndicateUser.Read( username )
+         
+         volume_id = VolumeIDCounter.next_value()
+         
+         volume_key_name = Volume.make_key_name( volume_id=volume_id )
+         volume_key = storagetypes.make_key( Volume, volume_key_name )
 
          # new volume
          volume = Volume( name=name,
-                        key=volume_key,
+                        key = volume_key,
                         blocksize=blocksize,
                         description=description,
                         owner_id=user.owner_id,
-                        volume_id=vid_counter.value,
+                        volume_id=volume_id,
                         active=kwargs.get('active',False),
                         version=1,
                         UG_version=1,
@@ -224,19 +253,36 @@ class Volume( storagetypes.Object ):
                         volume_secret_salted_hash = kwargs['volume_secret_salted_hash']
                         )
 
-         vol_future = volume.put_async()
+         volume.put()
+         
+         # verify that no other volumes with this name exist
+         existing_volumes = Volume.ListAll( {"Volume.name ==" : name} )
+         if len(existing_volumes) > 1:
+            # roll back
+            volume.key.delete()
+            raise Exception ("Volume '%s' already exists" % name)
 
-         storagetypes.wait_futures( [vid_future, vol_future] )
 
-         return volume.key
+         # add this Volume to this User
+         try:
+            storagetypes.transaction( lambda: SyndicateUser.add_volume_to_user(volume_id, username), retries=10 )
+         except Exception, e:
+            logging.exception( "__try_add_volume_to_user exception", e )
+
+            # roll back
+            volume.key.delete()
+
+            raise Exception( "System is under heavy load right now.  Please try again later." )
+            
+         return volume_key
+         
 
    @classmethod
-   def Read( cls, name ):
+   def Read( cls, volume_id ):
       """
-      Given a volume ID (name), get the volume entity. Returns None on miss.
+      Given a volume ID, get the volume entity. Returns None on miss.
       """
-      name = unicode(name).strip().replace(" ","_")
-      volume_key_name = Volume.make_key_name( name=name )
+      volume_key_name = Volume.make_key_name( volume_id=volume_id )
       volume_key = storagetypes.make_key( Volume, volume_key_name )
 
       volume = storagetypes.memcache.get( volume_key_name )
@@ -264,26 +310,23 @@ class Volume( storagetypes.Object ):
 
 
    @classmethod
-   def update_shard_count( cls, volume_name, num_shards, **txn_args ):
+   def update_shard_count( cls, volume_id, num_shards, **txn_args ):
       """
       Update the shard count of the volume, but in a transaction.
       """
-      volume_name = unicode(volume_name).replace(" ","_")
-      volume_key = storagetypes.make_key( Volume, Volume.make_key_name( name=volume_name ) )
+      volume_key = storagetypes.make_key( Volume, Volume.make_key_name( volume_id=volume_id ) )
       
       num_shards = storagetypes.transaction( lambda: __volume_update_shard_count( volume_key, num_shards ), **txn_args )
       
       return num_shards
 
-   # Changed volume_id to name in parameters - John
    @classmethod
-   def Update( cls, name, **fields ):
+   def Update( cls, volume_id, **fields ):
       '''
       Update volume identified by name with fields specified as a dictionary.
       '''
-      name = unicode(name).replace(" ","_")
-      volume = Volume.Read(name)
-      volume_key_name = Volume.make_key_name( name=name )
+      volume = Volume.Read(volume_id)
+      volume_key_name = Volume.make_key_name( volume_id=volume_id )
       storagetypes.memcache.delete(volume_key_name)
 
       for key, value in fields.iteritems():
@@ -296,21 +339,9 @@ class Volume( storagetypes.Object ):
          
 
    @classmethod
-   def Delete( cls, name ):
+   def Delete( cls, volume_id ):
       '''
       Delete volume from datastore.
       '''
-      name = unicode(name).replace(" ","_")
-      volume_key = storagetypes.make_key( Volume, Volume.make_key_name( name=volume_name ) )
+      volume_key = storagetypes.make_key( Volume, Volume.make_key_name( volume_id=volume_id ) )
       return volume_key.delete()
-
-      
-   @classmethod
-   def ListAll( cls, attrs ):
-      '''
-      attrs is a dictionary of Volume.PROPERTY [operator]: [value]
-      '''
-      qry = Volume.query()
-      ret = cls.ListAll_runQuery( qry, attrs )
-
-      return ret
