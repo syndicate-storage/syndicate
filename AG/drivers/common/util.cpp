f60dd77863a8910e2ad0bd202999356e1abb4371
wathsalav <wvithanage@gmail.com>
2013-08-05 19:42:35 -0400

    Adding common utils

---
+++ AG/drivers/common/util.cpp
@@ -0,0 +1,78 @@
+#include <util.h>
+
+//Global sighand set
+sighands sghs;
+
+void clean_dir(const char *dirname) {
+    if (dirname == NULL)
+	return;
+    DIR *dirp = opendir(dirname);
+    if (dirp == NULL) {
+	perror("opendir");
+	return;
+    }
+    struct dirent *dentry;
+    struct dirent *dentry_dp = NULL;
+    ssize_t len = offsetof(struct dirent, d_name) +
+				pathconf(dirname, _PC_NAME_MAX) + 1;
+    dentry = (struct dirent*)malloc(len);
+    while (readdir_r(dirp, dentry, &dentry_dp) ==  0 && 
+	    dentry_dp != NULL) {
+	if (strcmp(dentry->d_name, ".") && 
+		strcmp(dentry->d_name, "..")) {
+		ssize_t path_len = strlen(dirname) + strlen(dentry->d_name) + 2;
+		char *path = (char*)malloc(path_len);
+		memset(path, 0, path_len);
+		strcpy(path, dirname);
+		strcat(path, "/");
+		strcat(path, dentry->d_name);
+	    if (dentry->d_type == DT_DIR) {
+		clean_dir(path);
+		if (rmdir(path) < 0)
+		    perror("unlink - dir");
+	    }
+	    else {
+		if (unlink(path) < 0) 
+		    perror("unlink - file");
+	    }
+	    free(path);
+	    path = NULL;
+	}
+    }    
+    closedir(dirp);
+    return;
+}
+
+
+//Register signal handler with sighands
+sighandler_t add_signal_handler(int signum, sighandler_t hndl) {
+    sighandler_t rc = 0;
+    switch (signum) {
+	case SIGTERM:
+	    sghs.term_handler = hndl;
+	    rc = signal(signum, hndl);
+	    break;
+	case SIGINT:
+	    sghs.init_handler = hndl;
+	    rc = signal(signum, hndl);
+	    break;
+    }	
+    return rc;
+}
+
+//Remove signal handler sighands
+sighandler_t remove_signal_handler(int signum) {
+    sighandler_t rc = 0;
+    switch (signum) {
+	case SIGTERM:
+	    sghs.term_handler = NULL;
+	    rc = signal(signum, NULL);
+	    break;
+	case SIGINT:
+	    sghs.init_handler = NULL;
+	    rc = signal(signum, NULL);
+	    break;
+    }	
+    return rc;
+}
+
---
+++ AG/drivers/common/util.h
@@ -0,0 +1,37 @@
+#ifndef _UTIL_H_
+#define _UTIL_H_
+
+#include <iostream>
+
+#include <sys/types.h>
+#include <dirent.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+using namespace std;
+
+//sighand set
+struct _sighand_struct{
+    sighandler_t term_handler;
+    sighandler_t init_handler;
+    _sighand_struct():term_handler(NULL), init_handler(NULL){}
+};
+
+typedef _sighand_struct sighands;
+
+//Delete all the files in a given directory
+void clean_dir(const char *dir_name);
+
+//Register signal handler with sighands
+sighandler_t add_signal_handler(int signum, sighandler_t handler);
+
+//Remove signal handler sighands
+sighandler_t remove_signal_handler(int signum);
+
+#endif //_UTIL_H_
+
