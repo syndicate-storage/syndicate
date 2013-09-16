8dd23baf9bb31dea74fa63ddf77ba82e4724510c
jcnelson <jcnelson@cs.princeton.edu>
2013-09-16 00:03:09 -0400

    If there is no entry found, do not try to populate it with shards.

--- ms/MS/entry.py
+++ ms/MS/entry.py
@@ -867,8 +867,10 @@ class MSEntry( storagetypes.Object ):
    @classmethod
    @storagetypes.concurrent
    def __read_msentry_key_mapper( cls, volume_id, file_id, num_shards ):
-      # returns (memcache_key, ent), so the caller can go ahead and build a memcache dict from them
       msentry, shards = yield MSEntry.__read_msentry_base( volume_id, file_id ), MSEntry.__read_msentry_shards( volume_id, file_id, num_shards )
+      
+      if msentry == None:
+         storagetypes.concurrent_return(None, None)
       
       shards_existing = filter( lambda x: x != None, shards )
       msentry.populate_from_shards( shards_existing )
