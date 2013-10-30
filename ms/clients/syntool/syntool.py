417b867b0d756b942ca5517f8f5b53ffd4d31bf8
jcnelson <jcnelson@cs.princeton.edu>
2013-10-30 04:32:19 -0400

    * WIP: public API for the MS for client-side scripts * Authenticate() methods for Volume and Gateway * Call the client-side tool "syntool.py" instead of "synconfig.py"

---
+++ ms/clients/syntool/syntool.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+"""
+   Copyright 2013 The Trustees of Princeton University
+   All Rights Reserved
+"""
+
+import os
+import json
+
+import rpc.jsonrpc as jsonrpc
+
+import synconfig.common as common 
+import synconfig.conf as conf 
+
+import pprint 
+
+CONFIG = None
+
+# -------------------
