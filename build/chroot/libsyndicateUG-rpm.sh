e8a637d0f51075f4c8c60ea13356729c2b7977e5
root <root@t510.home>
2014-04-09 05:30:11 -0400

    Update RPM build scripts

--- build/chroot/libsyndicateUG-rpm.sh
+++ build/chroot/libsyndicateUG-rpm.sh
@@ -4,7 +4,7 @@ ROOT=$HOME/syndicate/libsyndicateUG-root
 NAME="libsyndicateUG"
 VERSION="0.$(date +%Y\%m\%d\%H\%M\%S)"
 
-DEPS="openssl curl protobuf libsyndicate" 
+DEPS="openssl curl protobuf libsyndicate fuse fuse-libs boost-system boost-thread" 
 
 DEPARGS=""
 for pkg in $DEPS; do
@@ -15,5 +15,5 @@ source /usr/local/rvm/scripts/rvm
 
 rm -f $NAME-0*.rpm
 
-fpm --force -s dir -t rpm -a $(uname -p) -v $VERSION -n $NAME $DEPARGS -C $ROOT --license "Apache 2.0" --vendor "Princeton University" --description "Syndicate client library, for creating embedded User Gateways." $(ls $ROOT)
+fpm --force -s dir -t rpm -a $(uname -p) -v $VERSION -n $NAME $DEPARGS -C $ROOT --license "Apache 2.0" --vendor "Princeton University" --description "Syndicate User Gateway library." $(ls $ROOT)
 
