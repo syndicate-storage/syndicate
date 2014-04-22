eaccbd24294ec270413e0ce08414629d99a5c237
root <root@t510.home>
2014-04-22 10:29:00 +0000

    Add python-syndicate as a dependency to syndicate-UG, since syndicate-UG now includes the syndicate-ug watchdog (which uses Syndicate's python package)

--- build/chroot/syndicate-UG-deb.sh
+++ build/chroot/syndicate-UG-deb.sh
@@ -4,7 +4,7 @@ ROOT=$HOME/syndicate/syndicate-UG-root
 NAME="syndicate-UG"
 VERSION="0.$(date +%Y\%m\%d\%H\%M\%S)"
 
-DEPS="libssl1.0.0 libcurl3-gnutls libprotobuf7 libsyndicate fuse libboost-system1.48.0 libboost-thread1.48.0" 
+DEPS="libssl1.0.0 libcurl3-gnutls libprotobuf7 libsyndicate fuse libboost-system1.48.0 libboost-thread1.48.0 python-syndicate" 
 
 DEPARGS=""
 for pkg in $DEPS; do
@@ -15,5 +15,5 @@ source /usr/local/rvm/scripts/rvm
 
 rm -f $NAME-0*.deb
 
-fpm --force -s dir -t deb -a $(uname -p) -v $VERSION -n $NAME $DEPARGS -C $ROOT --license "Apache 2.0" --vendor "Princeton University" --description "Syndicate User Gateway binaries.  Includes syndicatefs, syndicate-httpd, syndicate-ipc." $(ls $ROOT)
+fpm --force -s dir -t deb -a $(uname -p) -v $VERSION -n $NAME $DEPARGS -C $ROOT --license "Apache 2.0" --vendor "Princeton University" --description "Syndicate User Gateway binaries.  Includes syndicatefs, syndicate-httpd, syndicate-ipc, and syndicate-ug watchdog." $(ls $ROOT)
 
