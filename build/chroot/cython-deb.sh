0aeaf2e2ca43f68c489b1712ffede7611564ff5b
root <root@t510.(none)>
2014-04-28 19:00:22 +0000

    Added .deb-builder for Cython 0.19.2 (which is not in Ubuntu 12.04)

---
+++ build/chroot/cython-deb.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+ROOT=$HOME/syndicate/python-syndicate-root
+NAME="cython0.19"
+VERSION="0.19.2"
+
+DEPS="python python-support libc6" 
+
+DEPARGS=""
+for pkg in $DEPS; do
+   DEPARGS="$DEPARGS -d $pkg"
+done
+
+source /usr/local/rvm/scripts/rvm
+
+rm -f $NAME-0*.deb
+
+fpm --force -s dir -t deb -a $(uname -p) -v $VERSION -n $NAME $DEPARGS -C $ROOT --license "Apache 2.0" --vendor "Princeton University" --description "Cython 0.19.x for Ubuntu 12.04" $(ls $ROOT)
+
