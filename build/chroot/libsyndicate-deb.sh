14704bf7967e45d869b4678dc1c10e1cece4a1c7
jcnelson <jcnelson@cs.princeton.edu>
2014-09-02 23:39:06 -0400

    Update INSTALL and build scripts to include zlib as a dependency for libsyndicate.

--- build/chroot/libsyndicate-deb.sh
+++ build/chroot/libsyndicate-deb.sh
@@ -10,7 +10,7 @@ ROOT=$1
 NAME="libsyndicate"
 VERSION="0.$(date +%Y\%m\%d\%H\%M\%S)"
 
-DEPS="libssl1.0.0 libcurl3-gnutls libprotobuf7 libmicrohttpd0.9 libjson0" 
+DEPS="libssl1.0.0 libcurl3-gnutls libprotobuf7 libmicrohttpd0.9 libjson0 zlib1g" 
 
 DEPARGS=""
 for pkg in $DEPS; do
