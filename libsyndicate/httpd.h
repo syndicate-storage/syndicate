6009ec8e13a2b702187b706ad97ae90054f95553
Jude Nelson <judecn@gmail.com>
2015-05-04 22:52:17 -0400

    Much-improved refactored HTTP server implementation.  External interfaces are conceptuall the same (but with a few tweaks), but error-handling and documentation are much more explicit.  Moreover, the way we handle connections has been improved to allow for the notion of suspending and resuming connections, in order to make I/O-intensive workloads friendlier towards event-loops.

--- libsyndicate/httpd.h
+++ libsyndicate/httpd.h
@@ -69,13 +69,7 @@ using namespace std;
 #include "libsyndicate/libsyndicate.h"
 #include "microhttpd.h"
 
-
-// HTTP handler args
-struct md_HTTP_handler_args {
-   struct md_syndicate_conf* conf;
-   struct md_user_entry** users;
-};
-
+#define SG_HTTP_TMPFILE_FORMAT          "/tmp/.syndicate-upload.XXXXXX"
 
 // HTTP headers
 struct md_HTTP_header {
@@ -109,69 +103,74 @@ struct md_HTTP_response {
 
 struct md_HTTP;
 
+// HTTP post field data 
+struct SG_HTTP_post_field {
+   md_response_buffer_t* rb;    // for RAM upload 
+   
+   int tmpfd;                   // temporary file 
+   char* tmpfd_path;      
+};
+
+typedef map<string, struct SG_HTTP_post_field> SG_HTTP_post_field_map_t;
+
 // HTTP connection data
 struct md_HTTP_connection_data {
-   struct md_HTTP* http;
-   struct md_syndicate_conf* conf;
-   struct ms_client* ms;
-   struct MHD_PostProcessor* pp;
-   struct md_HTTP_response* resp;
-   struct md_HTTP_header** headers;
-   char* remote_host;
-   char const* method;
-   size_t content_length;
    
-   int status;
-   int mode;
+   struct md_HTTP* http;                // HTTP server reference
+   struct MHD_PostProcessor* pp;        // HTTP post processor 
+   struct md_HTTP_header** headers;     // headers from the request 
+   char* remote_host;                   // remote peer (host:port string)
+   uint64_t content_length;             // length of the content returned
+   
+   int status;                // HTTP status
+   int mode;                  // numerical alias for the operation
    off_t offset;              // if there isn't a post-processor, then this stores the offset 
    
    void* cls;                 // user-supplied closure
-   char* version;             // HTTP version
+   int version;               // HTTP version
    char* url_path;            // path requested
-   char* query_string;        // url's query string
-   md_response_buffer_t* rb;     // response buffer for small messages
+   char* query_string;        // url's query string (points to data alloc'ed to url_path)
+   
+   SG_HTTP_post_field_map_t* post_fields;       // upload data keyed by field
+   
+   struct MHD_Connection* connection;   // reference to HTTP connection 
+   volatile bool suspended;             // if true, then this connection is suspended
+   struct md_HTTP_response* resume_resp;        // if set, this is the response to send back immediately following a connection resume
 };
 
-// gateway request structure
-struct md_gateway_request_data {
-   uint64_t volume_id;
-   uint64_t file_id;
-   char* fs_path;
-   int64_t file_version;
-   uint64_t block_id;
-   int64_t block_version;
-   struct timespec manifest_timestamp;
-};
+typedef int (*SG_HTTP_method_t)( struct md_HTTP_connection_data*, struct md_HTTP_response* );
+typedef int (*SG_HTTP_post_field_handler_t)( char const*, char const*, char const*, off_t, size_t, void* );
+
+typedef map<string, SG_HTTP_post_field_handler_t> SG_HTTP_post_field_handler_map_t;
 
 // HTTP callbacks and control code
 struct md_HTTP {
-
-   pthread_rwlock_t lock;
    
    struct MHD_Daemon* http_daemon;
    int server_type;   // one of the MHD options
+   
+   bool running;
 
+   // TLS key pair
    char* server_cert;
    char* server_pkey;
    
-   void*                     (*HTTP_connect)( struct md_HTTP_connection_data* md_con_data );
-   uint64_t                  (*HTTP_authenticate)( struct md_HTTP_connection_data* md_con_data, char* username, char* password );
-   struct md_HTTP_response*  (*HTTP_HEAD_handler)( struct md_HTTP_connection_data* md_con_data );
-   struct md_HTTP_response*  (*HTTP_GET_handler)( struct md_HTTP_connection_data* md_con_data );
-   int                       (*HTTP_POST_iterator)(void *coninfo_cls, enum MHD_ValueKind kind, 
-                                                   char const *key,
-                                                   char const *filename, char const *content_type,
-                                                   char const *transfer_encoding, char const *data, 
-                                                   uint64_t off, size_t size);
-   void                      (*HTTP_POST_finish)( struct md_HTTP_connection_data* md_con_data );
-   int                       (*HTTP_PUT_iterator)(void *coninfo_cls, enum MHD_ValueKind kind,
-                                                  char const *key,
-                                                  char const *filename, char const *content_type,
-                                                  char const *transfer_encoding, char const *data,
-                                                  uint64_t off, size_t size);
-   void                      (*HTTP_PUT_finish)( struct md_HTTP_connection_data* md_con_data );
-   struct md_HTTP_response*  (*HTTP_DELETE_handler)( struct md_HTTP_connection_data* md_con_data, int depth );
-   void                      (*HTTP_cleanup)(struct MHD_Connection *connection, void *con_cls, enum MHD_RequestTerminationCode term);
+   // connection setup/cleanup
+   int  (*HTTP_connect)( struct md_HTTP_connection_data* md_con_data, void** cls );
+   void (*HTTP_cleanup)( void* cls );
+   
+   // associated server-wide state to make available to connections
+   void* server_cls;
+   
+   // method handlers
+   SG_HTTP_method_t HTTP_HEAD_handler;
+   SG_HTTP_method_t HTTP_GET_handler;
+   SG_HTTP_method_t HTTP_POST_finish;
+   SG_HTTP_method_t HTTP_PUT_finish;
+   SG_HTTP_method_t HTTP_DELETE_handler;
+   
+   // upload field handlers
+   SG_HTTP_post_field_handler_map_t* upload_field_handlers;
 };
 
 extern char const MD_HTTP_NOMSG[128];
@@ -192,7 +191,8 @@ extern char const MD_HTTP_504_MSG[128];
 
 extern char const MD_HTTP_DEFAULT_MSG[128];
 
-#define SG_HTTP_TRYAGAIN_MSG MD_HTTP_503_MSG
+#define SG_HTTP_TRYAGAIN            503
+#define SG_HTTP_TRYAGAIN_MSG        MD_HTTP_503_MSG
 
 #define MD_HTTP_TYPE_STATEMACHINE   MHD_USE_SELECT_INTERNALLY
 #define MD_HTTP_TYPE_THREAD         MHD_USE_THREAD_PER_CONNECTION
@@ -200,28 +200,21 @@ extern char const MD_HTTP_DEFAULT_MSG[128];
 #define MD_MIN_POST_DATA 4096
 
 #define md_HTTP_connect( http, callback ) (http).HTTP_connect = (callback)
-#define md_HTTP_authenticate( http, callback ) (http.HTTP_authenticate) = (callback)
 #define md_HTTP_GET( http, callback ) (http).HTTP_GET_handler = (callback)
 #define md_HTTP_HEAD( http, callback ) (http).HTTP_HEAD_handler = (callback)
-#define md_HTTP_POST_iterator( http, callback ) (http).HTTP_POST_iterator = (callback)
+#define md_HTTP_upload_iterator( http, callback ) (http).HTTP_upload_iterator = (callback)
 #define md_HTTP_POST_finish( http, callback ) (http).HTTP_POST_finish = (callback)
-#define md_HTTP_PUT_iterator( http, callback ) (http).HTTP_PUT_iterator = (callback)
 #define md_HTTP_PUT_finish( http, callback ) (http).HTTP_PUT_finish = (callback)
 #define md_HTTP_DELETE( http, callback ) (http).HTTP_DELETE_handler = (callback)
 #define md_HTTP_close( http, callback ) (http).HTTP_cleanup = (callback)
 #define md_HTTP_server_type( http, type ) (http).server_type = type
+#define md_HTTP_post_field_handler( http, field, callback ) (*(http).upload_field_handlers)[ string(field) ] = (callback)
 
 // use STRONG encryption, with perfect forward security
 // Use ephemeral Diffie-Hellman for key exchange (RSA or ECC)
 // Use at least 256-bit MACs
 // Use at least 256-bit keys for data encryption.
 #define SYNDICATE_GNUTLS_CIPHER_SUITES "PFS:-ARCFOUR-128:-3DES-CBC:-AES-128-CBC:-AES-256-CBC:-CAMELLIA-128-CBC:-MD5:-SHA1"
-
-// authentication (can be OR'ed together)
-#define HTTP_AUTHENTICATE_NONE        0
-#define HTTP_AUTHENTICATE_READ        1
-#define HTTP_AUTHENTICATE_WRITE       2
-#define HTTP_AUTHENTICATE_READWRITE   3
 
 // types of responses
 #define HTTP_RESPONSE_RAM              1
@@ -240,48 +233,50 @@ extern char const MD_HTTP_DEFAULT_MSG[128];
 
 #define MD_HTTP_UNKNOWN  -1
 
-// basic helpers
-int md_HTTP_rlock( struct md_HTTP* http );
-int md_HTTP_wlock( struct md_HTTP* http );
-int md_HTTP_unlock( struct md_HTTP* http );
+extern "C" {
 
-// default callbacks and helpers
-int md_response_buffer_upload_iterator(void *coninfo_cls, enum MHD_ValueKind kind,
-                                       const char *key,
-                                       const char *filename, const char *content_type,
-                                       const char *transfer_encoding, const char *data,
-                                       uint64_t off, size_t size);
+// POST field handlers 
+int md_HTTP_post_field_handler_ram( char const* field_name, char const* filename, char const* data, off_t offset, size_t len, void* cls );
+int md_HTTP_post_field_handler_disk( char const* field_name, char const* filename, char const* data, off_t offset, size_t len, void* cls );
 
 // HTTP server responses
-int md_create_HTTP_response_ram( struct md_HTTP_response* resp, char const* mimetype, int status, char const* data, int len );
-int md_create_HTTP_response_ram_nocopy( struct md_HTTP_response* resp, char const* mimetype, int status, char const* data, int len );
-int md_create_HTTP_response_ram_static( struct md_HTTP_response* resp, char const* mimetype, int status, char const* data, int len );
-int md_create_HTTP_response_fd( struct md_HTTP_response* resp, char const* mimetype, int status, int fd, off_t offset, size_t size );
-int md_create_HTTP_response_stream( struct md_HTTP_response* resp, char const* mimetype, int status, uint64_t size, size_t blk_size, md_HTTP_stream_callback scb, void* cls, md_HTTP_free_cls_callback fcb );
-void md_free_HTTP_response( struct md_HTTP_response* resp );
+int md_HTTP_create_response_ram( struct md_HTTP_response* resp, char const* mimetype, int status, char const* data, int len );
+int md_HTTP_create_response_ram_nocopy( struct md_HTTP_response* resp, char const* mimetype, int status, char const* data, int len );
+int md_HTTP_create_response_ram_static( struct md_HTTP_response* resp, char const* mimetype, int status, char const* data, int len );
+int md_HTTP_create_response_fd( struct md_HTTP_response* resp, char const* mimetype, int status, int fd, off_t offset, size_t size );
+int md_HTTP_create_response_stream( struct md_HTTP_response* resp, char const* mimetype, int status, uint64_t size, size_t blk_size, md_HTTP_stream_callback scb, void* cls, md_HTTP_free_cls_callback fcb );
+int md_HTTP_create_response_builtin( struct md_HTTP_response* resp, int status );
+void md_HTTP_response_free( struct md_HTTP_response* resp );
 
-// get/set closure
-void* md_cls_get( void* cls );
-void md_cls_set_status( void* cls, int status );
-struct md_HTTP_response* md_cls_set_response( void* cls, struct md_HTTP_response* resp );
+// suspend/resume connection 
+int md_HTTP_connection_suspend( struct md_HTTP_connection_data* con_data );
+int md_HTTP_connection_resume( struct md_HTTP_connection_data* con_data, struct md_HTTP_response* resp );
 
 // init/start/stop/shutdown
-int md_HTTP_init( struct md_HTTP* http, int server_type );
-int md_start_HTTP( struct md_HTTP* http, int portnum, struct md_syndicate_conf* conf );
-int md_stop_HTTP( struct md_HTTP* http );
-int md_free_HTTP( struct md_HTTP* http );
+int md_HTTP_init( struct md_HTTP* http, int server_type, void* server_cls );
+int md_HTTP_start( struct md_HTTP* http, int portnum, struct md_syndicate_conf* conf );
+int md_HTTP_stop( struct md_HTTP* http );
+int md_HTTP_free( struct md_HTTP* http );
 
 // headers
-void md_create_HTTP_header( struct md_HTTP_header* header, char const* h, char const* value );
-void md_free_HTTP_header( struct md_HTTP_header* header );
-char const* md_find_HTTP_header( struct md_HTTP_header** headers, char const* header );
-int md_HTTP_add_header( struct md_HTTP_response* resp, char const* header, char const* value );
+int md_HTTP_header_create( struct md_HTTP_header* header, char const* h, char const* value );
+void md_HTTP_header_free( struct md_HTTP_header* header );
+char const* md_HTTP_header_lookup( struct md_HTTP_header** headers, char const* header );
+int md_HTTP_header_add( struct md_HTTP_response* resp, char const* header, char const* value );
 
 // path parsing 
 int md_HTTP_parse_url_path( char const* _url_path, uint64_t* _volume_id, char** _file_path, uint64_t* _file_id, int64_t* _file_version, uint64_t* _block_id, int64_t* _block_version, struct timespec* _manifest_ts );
 
 // memory management
 void md_HTTP_free_connection_data( struct md_HTTP_connection_data* con_data );
-void md_gateway_request_data_free( struct md_gateway_request_data* reqdat );
+
+// get uploaded data 
+int md_HTTP_upload_get_field_buffer( struct md_HTTP_connection_data* con_data, char const* field_name, char** buf, size_t* buflen );
+int md_HTTP_upload_get_field_tmpfile( struct md_HTTP_connection_data* con_data, char const* field_name, char** path, int* fd );
+
+// getters 
+void* md_HTTP_cls( struct md_HTTP* http );
+
+}
 
 #endif
