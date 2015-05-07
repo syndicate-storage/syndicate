64d9889849ccf3c915e070bead1275422b5c2f10
Jude Nelson <judecn@gmail.com>
2015-05-07 04:03:33 -0400

    Add Makefile target for MS, and all ancillery data

--- buildconf.mk
+++ buildconf.mk
@@ -1,47 +1,69 @@
 # build environment
-ROOT_DIR	:= $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
-BUILD		?= $(ROOT_DIR)/build/out
-DISTRO	?= DEBIAN
+ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+BUILD    ?= $(ROOT_DIR)/build/out
+DISTRO   ?= DEBIAN
 
 # install environment
-PREFIX			?= /
-BINDIR			?= $(PREFIX)/bin
-LIBDIR			?= $(PREFIX)/lib
-INCLUDEDIR		?= $(PREFIX)/include
-PKGCONFIGDIR	?= $(PREFIX)/lib/pkgconfig
+PREFIX         ?= /
+BINDIR         ?= $(PREFIX)/bin
+LIBDIR         ?= $(PREFIX)/lib
+INCLUDEDIR     ?= $(PREFIX)/include
+PKGCONFIGDIR   ?= $(PREFIX)/lib/pkgconfig
 
 # protobufs 
-BUILD_PROTOBUFS_CPP		:= $(BUILD)/protobufs/cpp/
-BUILD_PROTOBUFS_CPP_H	:= $(wildcard $(BUILD)/protobufs/cpp/%.h)
-BUILD_PROTOBUFS_PYTHON	:= $(BUILD)/protobufs/python/
-BUILD_PROTOBUFS_DIRS		:= $(BUILD_PROTOBUFS_CPP) $(BUILD_PROTOBUFS_PYTHON)
+BUILD_PROTOBUFS_CPP     := $(BUILD)/protobufs/cpp/
+BUILD_PROTOBUFS_CPP_H   := $(wildcard $(BUILD)/protobufs/cpp/%.h)
+BUILD_PROTOBUFS_PYTHON  := $(BUILD)/protobufs/python/
+BUILD_PROTOBUFS_DIRS    := $(BUILD_PROTOBUFS_CPP) $(BUILD_PROTOBUFS_PYTHON)
+
+# metadata service 
+BUILD_MS                := $(BUILD)/ms
+BUILD_MS_DIRS           := $(BUILD_MS)/common \
+                           $(BUILD_MS)/protobufs \
+                           $(BUILD_MS)/storage \
+                           $(BUILD_MS)/storage/backends \
+                           $(BUILD_MS)/MS \
+                           $(BUILD_MS)/MS/methods \
+                           $(BUILD_MS)/google \
+                           $(BUILD_MS)/google/protobuf \
+                           $(BUILD_MS)/google/protobuf/internal \
+                           $(BUILD_MS)/google/protobuf/compiler \
+                           $(BUILD_MS)/openid \
+                           $(BUILD_MS)/openid/server \
+                           $(BUILD_MS)/openid/extensions \
+                           $(BUILD_MS)/openid/extensions/draft \
+                           $(BUILD_MS)/openid/store \
+                           $(BUILD_MS)/openid/consumer \
+                           $(BUILD_MS)/openid/yadis
 
 # libsyndicate
-BUILD_LIBSYNDICATE		:= $(BUILD)/lib/libsyndicate
-BUILD_LIBSYNDICATE_DIRS	:= $(BUILD_LIBSYNDICATE)/ms $(BUILD_LIBSYNDICATE)/scrypt $(BUILD_LIBSYNDICATE)/drivers
+BUILD_LIBSYNDICATE       := $(BUILD)/lib/libsyndicate
+BUILD_LIBSYNDICATE_DIRS  := $(BUILD_LIBSYNDICATE)/ms $(BUILD_LIBSYNDICATE)/scrypt $(BUILD_LIBSYNDICATE)/drivers
 
 # user gateway 
-BUILD_UG			:= $(BUILD)/UG
-BUILD_UG_TOOLS := $(BUILD_UG)/tools
+BUILD_UG          := $(BUILD)/UG
+BUILD_UG_TOOLS    := $(BUILD_UG)/tools
 BUILD_UG_GATEWAYS := $(BUILD_UG)/gateways
-BUILD_UG_DIRS	:= $(BUILD_UG_TOOLS) $(BUILD_UG_GATEWAYS)
+BUILD_UG_DIRS     := $(BUILD_UG_TOOLS) $(BUILD_UG_GATEWAYS)
 
 # compiler
-CPP		:= g++ -Wall -g -fPIC -fstack-protector -fstack-protector-all -pthread
-INC		:= -I. -I$(ROOT_DIR) -I$(BUILD_PROTOBUFS_CPP) -I/usr/include
-DEFS		:= -D_THREAD_SAFE -D__STDC_FORMAT_MACROS -D_DISTRO_$(DISTRO)
-LIBINC	:= -L$(BUILD_LIBSYNDICATE)
+CPPFLAGS := -Wall -g -fPID -fstack-protector -fstack-protector-all -pthread
+CPP      := g++ $(CPPFLAGS)
+INC      := -I. -I$(ROOT_DIR) -I$(BUILD_PROTOBUFS_CPP)
+DEFS     := -D_THREAD_SAFE -D__STDC_FORMAT_MACROS -D_DISTRO_$(DISTRO)
+LIBINC   := -L$(BUILD_LIBSYNDICATE)
 
 # build setup
-BUILD_DIRS	:= $(BUILD_PROTOBUFS_DIRS) \
-					$(BUILD_LIBSYNDICATE_DIRS) \
-					$(BUILD_UG_DIRS)
+BUILD_DIRS   := $(BUILD_PROTOBUFS_DIRS) \
+                $(BUILD_MS_DIRS) \
+                $(BUILD_LIBSYNDICATE_DIRS) \
+                $(BUILD_UG_DIRS)
 
 # misc
 ifeq ($(DISTRO),DEBIAN)
-	LIBJSON ?= json-c
+   LIBJSON ?= json-c
 else
-	LIBJSON ?= json
+   LIBJSON ?= json
 endif
 
 all:
@@ -49,7 +71,7 @@ all:
 build_setup: $(BUILD_DIRS)
 
 $(BUILD_DIRS):
-	mkdir -p $@
+	@mkdir -p $@
 
 # debugging...
 print-%: ; @echo $*=$($*)
