4570c88ad66f91b7b977989824ce24f2848f4122
Jude Nelson <judecn@gmail.com>
2015-05-07 23:45:42 -0400

    Update Makefiles to (1) create all files as explicit targets, and (2) not depend on directories (instead, make them as part of fulfilling each target)

--- buildconf.mk
+++ buildconf.mk
@@ -2,9 +2,12 @@
 ROOT_DIR := $(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
 BUILD    ?= $(ROOT_DIR)/build/out
 DISTRO   ?= DEBIAN
+BUILD_BINDIR := $(BUILD)/bin
+BUILD_LIBDIR := $(BUILD)/lib
+BUILD_INCLUDEDIR := $(BUILD)/include/
 
 # install environment
-PREFIX         ?= /
+PREFIX         ?= /usr/local
 BINDIR         ?= $(PREFIX)/bin
 LIBDIR         ?= $(PREFIX)/lib
 INCLUDEDIR     ?= $(PREFIX)/include
@@ -12,8 +15,8 @@ PKGCONFIGDIR   ?= $(PREFIX)/lib/pkgconfig
 
 # protobufs 
 BUILD_PROTOBUFS_CPP     := $(BUILD)/protobufs/cpp/
-BUILD_PROTOBUFS_CPP_H   := $(wildcard $(BUILD)/protobufs/cpp/%.h)
 BUILD_PROTOBUFS_PYTHON  := $(BUILD)/protobufs/python/
+BUILD_PROTOBUFS_INCLUDEDIR := $(BUILD_INCLUDEDIR)/libsyndicate
 BUILD_PROTOBUFS_DIRS    := $(BUILD_PROTOBUFS_CPP) $(BUILD_PROTOBUFS_PYTHON)
 
 # metadata service 
@@ -37,19 +40,27 @@ BUILD_MS_DIRS           := $(BUILD_MS)/common \
                            $(BUILD_MS)/openid/yadis
 
 # libsyndicate
-BUILD_LIBSYNDICATE       := $(BUILD)/lib/libsyndicate
-BUILD_LIBSYNDICATE_DIRS  := $(BUILD_LIBSYNDICATE)/ms $(BUILD_LIBSYNDICATE)/scrypt $(BUILD_LIBSYNDICATE)/drivers
+BUILD_LIBSYNDICATE       := $(BUILD_LIBDIR)
+BUILD_LIBSYNDICATE_INCLUDEDIR := $(BUILD_INCLUDEDIR)/libsyndicate
+BUILD_LIBSYNDICATE_DIRS  := $(BUILD_LIBSYNDICATE)/ms \
+	                         $(BUILD_LIBSYNDICATE)/scrypt \
+									 $(BUILD_LIBSYNDICATE)/drivers \
+                            $(BUILD_LIBSYNDICATE_INCLUDEDIR) \
+									 $(BUILD_LIBSYNDICATE_INCLUDEDIR)/ms \
+									 $(BUILD_LIBSYNDICATE_INCLUDEDIR)/scrypt \
+									 $(BUILD_LIBSYNDICATE_INCLUDEDIR)/drivers
 
 # user gateway 
 BUILD_UG          := $(BUILD)/UG
 BUILD_UG_TOOLS    := $(BUILD_UG)/tools
 BUILD_UG_GATEWAYS := $(BUILD_UG)/gateways
-BUILD_UG_DIRS     := $(BUILD_UG_TOOLS) $(BUILD_UG_GATEWAYS)
+BUILD_UG_INCLUDEDIR := $(BUILD_INCLUDEDIR)/syndicate-ug
+BUILD_UG_DIRS     := $(BUILD_UG_TOOLS) $(BUILD_UG_GATEWAYS) $(BUILD_UG_INCLUDEDIR)
 
 # compiler
-CPPFLAGS := -Wall -g -fPID -fstack-protector -fstack-protector-all -pthread
+CPPFLAGS := -Wall -g -fPIC -fstack-protector -fstack-protector-all -pthread
 CPP      := g++ $(CPPFLAGS)
-INC      := -I. -I$(ROOT_DIR) -I$(BUILD_PROTOBUFS_CPP)
+INC      := -I. -I$(ROOT_DIR) -I$(BUILD_INCLUDEDIR)
 DEFS     := -D_THREAD_SAFE -D__STDC_FORMAT_MACROS -D_DISTRO_$(DISTRO)
 LIBINC   := -L$(BUILD_LIBSYNDICATE)
 
