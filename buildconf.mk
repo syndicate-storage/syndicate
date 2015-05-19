72ba4979666575aa2e656272f1ea59a6a5797c31
Jude Nelson <judecn@gmail.com>
2015-05-19 17:32:00 -0400

    Add PREFIX and DESTDIR variables; build all executables to build/out/bin

--- buildconf.mk
+++ buildconf.mk
@@ -7,11 +7,12 @@ BUILD_LIBDIR := $(BUILD)/lib
 BUILD_INCLUDEDIR := $(BUILD)/include/
 
 # install environment
-PREFIX         ?= /usr/local
-BINDIR         ?= $(PREFIX)/bin
-LIBDIR         ?= $(PREFIX)/lib
-INCLUDEDIR     ?= $(PREFIX)/include
-PKGCONFIGDIR   ?= $(PREFIX)/lib/pkgconfig
+PREFIX         ?= /
+DESTDIR			?= /usr/local
+BINDIR         ?= $(DESTDIR)$(PREFIX)/bin
+LIBDIR         ?= $(DESTDIR)$(PREFIX)/lib
+INCLUDEDIR     ?= $(DESTDIR)$(PREFIX)/include
+PKGCONFIGDIR   ?= $(DESTDIR)$(PREFIX)/lib/pkgconfig
 
 # protobufs 
 BUILD_PROTOBUFS_CPP     := $(BUILD)/protobufs/cpp/
@@ -21,6 +22,7 @@ BUILD_PROTOBUFS_DIRS    := $(BUILD_PROTOBUFS_CPP) $(BUILD_PROTOBUFS_PYTHON)
 
 # metadata service 
 BUILD_MS                := $(BUILD)/ms
+BUILD_MS_TOOLS				:= $(BUILD_BINDIR)
 BUILD_MS_DIRS           := $(BUILD_MS)/common \
                            $(BUILD_MS)/protobufs \
                            $(BUILD_MS)/storage \
@@ -51,9 +53,9 @@ BUILD_LIBSYNDICATE_DIRS  := $(BUILD_LIBSYNDICATE)/ms \
                             $(BUILD_LIBSYNDICATE_INCLUDEDIR)/drivers
 
 # user gateway 
-BUILD_UG          := $(BUILD)/UG
-BUILD_UG_TOOLS    := $(BUILD_UG)/tools
-BUILD_UG_GATEWAYS := $(BUILD_UG)/gateways
+BUILD_UG          := $(BUILD_BINDIR)
+BUILD_UG_TOOLS    := $(BUILD_BINDIR)
+BUILD_UG_GATEWAYS := $(BUILD_BINDIR)
 BUILD_UG_LIBS     := $(BUILD_LIBDIR)
 BUILD_UG_INCLUDEDIR := $(BUILD_INCLUDEDIR)/syndicate-ug
 BUILD_UG_DIRS     := $(BUILD_UG_TOOLS) \
@@ -68,16 +70,16 @@ BUILD_PYTHON_SYNDICATE_DIRS := $(BUILD_PYTHON_SYNDICATE)
 # compiler
 CPPFLAGS := -Wall -g -fPIC -fstack-protector -fstack-protector-all -pthread
 CPP      := g++ $(CPPFLAGS)
-INC      := -I. -I$(ROOT_DIR) -I$(BUILD_INCLUDEDIR)
+INC      := -I. -I$(BUILD_INCLUDEDIR) -I$(ROOT_DIR)
 DEFS     := -D_THREAD_SAFE -D__STDC_FORMAT_MACROS -D_DISTRO_$(DISTRO)
 LIBINC   := -L$(BUILD_LIBSYNDICATE)
 
 # build setup
-BUILD_DIRS   := $(BUILD_PROTOBUFS_DIRS) \
+BUILD_DIRS   := $(sort $(BUILD_PROTOBUFS_DIRS) \
                 $(BUILD_MS_DIRS) \
                 $(BUILD_LIBSYNDICATE_DIRS) \
                 $(BUILD_UG_DIRS) \
-                $(BUILD_PYTHON_SYNDICATE_DIRS)
+                $(BUILD_PYTHON_SYNDICATE_DIRS))
 
 # misc
 ifeq ($(DISTRO),DEBIAN)
