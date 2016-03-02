58bf85a4eee8fb7d1371cfcd6acb2f0d383205f8
Illyoung Choi <iychoi@email.arizona.edu>
2016-03-01 17:51:02 -0700

    Replace msconfig.py to not use localhost

--- docker/ms/build.sh
+++ docker/ms/build.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 admin_email"
-    exit 1
+ADMIN_EMAIL=""
+MS_HOST="localhost"
+if [ "$#" -ge 1 ]; then
+    ADMIN_EMAIL=$1
+fi
+
+if [ "$#" -ge 2 ]; then
+    MS_HOST=$2
 fi
 
 # copy from template
 cp Dockerfile.template Dockerfile
-PARAM='s/\$MS_APP_ADMIN_EMAIL\$/'$1'/g'
+PARAM='s/\$MS_APP_ADMIN_EMAIL\$/'$ADMIN_EMAIL'/g'
+sed -i $PARAM Dockerfile
+PARAM='s/\$MS_HOST\$/'$MS_HOST'/g'
 sed -i $PARAM Dockerfile
 
 #docker build --no-cache -t docker-ms .
